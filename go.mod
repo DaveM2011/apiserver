094192e2737c1e534b9d053f3106357ecebd6857
DaveM2011 <davem.2011@gmail.com>
2020-01-19 14:18:43 +0000

    Some setup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/davem2011/apiserver
+
+go 1.13
+
+require github.com/caddyserver/caddy/v2 v2.0.0-beta9
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	caddycmd "github.com/caddyserver/caddy/v2/cmd"
+
+	// plug in Caddy modules here
+	_ "github.com/caddyserver/caddy/v2/modules/standard"
+	_ "github.com/davem2011/apiserver/modules/visitorip"
+	_ "github.com/davem2011/apiserver/modules/booking_api"
+)
+
+func main() {
+	caddycmd.Main()
+}
